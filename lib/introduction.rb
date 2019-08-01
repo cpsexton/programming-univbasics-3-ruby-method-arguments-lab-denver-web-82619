8f278bdf1e190294f570ff3b4814285e6052bc64
Christopher Sexton <cpsexton@gmail.com>
2019-08-01 17:03:46 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -14,5 +14,5 @@ def introduction_with_language_optional(name, language)
   if language != TRUE
     language = "Ruby"
   else
-    put "Hi, my name is #{name} and I am learning to program in #{language}."
+    puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
