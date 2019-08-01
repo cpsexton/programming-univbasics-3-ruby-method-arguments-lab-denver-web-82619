f1e338d2c85f3cace28608dd40af94b81a718374
Christopher Sexton <cpsexton@gmail.com>
2019-08-01 18:07:34 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -13,4 +13,4 @@ introduction_with_language("Chris", "Ruby")
 def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-introduction_with_language_optional("Edwin", "Python")
+introduction_with_language_optional("Edwin")
